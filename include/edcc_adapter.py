7f633b1c91ffc714eb2c72cffe7e47ff9f475b3f
henan.wang <henan.wang@henan.wang's MacBook Pro>
2019-01-25 12:10:35 +0800

    add GetEnhanceImage

--- include/edcc_adapter.py
+++ include/edcc_adapter.py
@@ -5,8 +5,48 @@ from ctypes import *
 
 class EdccApi(object):
     def __init__(self):
-        self._edcc_lib_name = "libedcc.so"
+        self._edcc_lib_name = "libedcc.so"  # For Mac, libedcc.dylib, for linux libedcc.so
         self._edcc_lib = CDLL(self._edcc_lib_name)
+
+    '''
+        @in str:palmprint_image_path str:config_path int:coding_buffer_max_len
+        @out bytes:edcc_coding_buffer int:buffer_len
+        '''
+
+    def GetEnhanceImage(self,
+                      palmprint_image_path,
+                      palmprint_image_out_path,
+                      config_path,
+                      coding_buffer_max_len=1024*256):  # default 1024 * 32
+        #print('palmprint_image_path:', type(palmprint_image_path))
+        #print('palmprint_image_out_path:', type(palmprint_image_out_path))
+        #print('config_path:', type(config_path))
+        #print('coding_buffer_max_len:', type(coding_buffer_max_len))
+        if not isinstance(palmprint_image_path, str) \
+                or not isinstance(palmprint_image_out_path, str) \
+                or not isinstance(config_path, str) \
+                or not isinstance(coding_buffer_max_len, int):
+            raise RuntimeError("EDCC API:GetEnhanceImage. Params in error")
+        #c_pointer_palmprint_image_path = c_char_p(bytes(palmprint_image_path, encoding="utf-8"))
+        #c_pointer_palmprint_image_out_path = c_char_p(bytes(palmprint_image_out_path, encoding="utf-8"))
+        #c_pointer_config_path = c_char_p(bytes(config_path, encoding="utf-8"))
+        c_pointer_palmprint_image_path = c_char_p(bytes(palmprint_image_path))
+        c_pointer_palmprint_image_out_path = c_char_p(bytes(palmprint_image_out_path))
+        c_pointer_config_path = c_char_p(bytes(config_path))
+        c_coding_buffer = create_string_buffer(coding_buffer_max_len)
+        c_buffer_max_len = c_uint(coding_buffer_max_len)
+        buffer_len = c_uint(0)
+        c_pointer_buffer_len = POINTER(c_uint)(buffer_len)
+        iterface_ret = self._edcc_lib.GetEnhanceImage(c_pointer_palmprint_image_path,
+                                                    c_pointer_palmprint_image_out_path,
+                                                    c_pointer_config_path,
+                                                    c_buffer_max_len,
+                                                    c_coding_buffer,
+                                                    c_pointer_buffer_len)
+        if iterface_ret != 0:
+            raise RuntimeError("EDCC API:GetEnhanceImage. Call API result error[%d]." % (iterface_ret))
+        buffer_len_ret = c_pointer_buffer_len.contents.value
+        return c_coding_buffer.raw[:buffer_len_ret], buffer_len_ret
 
     '''
     @in str:palmprint_image_path str:config_path int:coding_buffer_max_len
@@ -15,13 +55,15 @@ class EdccApi(object):
     def GetEDCCCoding(self,
                       palmprint_image_path,
                       config_path,
-                      coding_buffer_max_len = 1024 * 32):
+                      coding_buffer_max_len = 1024*256):  # default 1024 * 32
         if not isinstance(palmprint_image_path, str) \
             or not isinstance(config_path, str) \
             or not isinstance(coding_buffer_max_len, int):
             raise RuntimeError("EDCC API:GetEDCCCoding. Params in error")
-        c_pointer_palmprint_image_path = c_char_p(bytes(palmprint_image_path, encoding="utf-8"))
-        c_pointer_config_path = c_char_p(bytes(config_path, encoding="utf-8"))
+        #c_pointer_palmprint_image_path = c_char_p(bytes(palmprint_image_path, encoding="utf-8"))
+        #c_pointer_config_path = c_char_p(bytes(config_path, encoding="utf-8"))
+        c_pointer_palmprint_image_path = c_char_p(bytes(palmprint_image_path))
+        c_pointer_config_path = c_char_p(bytes(config_path))
         c_coding_buffer = create_string_buffer(coding_buffer_max_len)
         c_buffer_max_len = c_uint(coding_buffer_max_len)
         buffer_len = c_uint(0)
@@ -68,9 +110,12 @@ class EdccApi(object):
             or not isinstance(secondpalmprint_image_path, str) \
             or not isinstance(config_path, str):
             raise RuntimeError("EDCC API:GetTwoPalmprintMatchScore. Params in error")
-        c_pointer_firstpalmprint_image_path = c_char_p(bytes(firstpalmprint_image_path, encoding="utf-8"))
-        c_pointer_secondpalmprint_image_path = c_char_p(bytes(secondpalmprint_image_path, encoding="utf-8"))
-        c_pointer_config_path = c_char_p(bytes(config_path, encoding="utf-8"))
+        #c_pointer_firstpalmprint_image_path = c_char_p(bytes(firstpalmprint_image_path, encoding="utf-8"))
+        #c_pointer_secondpalmprint_image_path = c_char_p(bytes(secondpalmprint_image_path, encoding="utf-8"))
+        #c_pointer_config_path = c_char_p(bytes(config_path, encoding="utf-8"))
+        c_pointer_firstpalmprint_image_path = c_char_p(bytes(firstpalmprint_image_path))
+        c_pointer_secondpalmprint_image_path = c_char_p(bytes(secondpalmprint_image_path))
+        c_pointer_config_path = c_char_p(bytes(config_path))
         match_score = c_double(0.0)
         c_pointer_match_score = POINTER(c_double)(match_score)
         iterface_ret = self._edcc_lib.GetTwoPalmprintMatchScore(c_pointer_firstpalmprint_image_path,
